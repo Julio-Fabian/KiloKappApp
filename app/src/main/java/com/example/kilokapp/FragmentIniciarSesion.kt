b49301456202ad7183ad9d92257bb8ff87ef2046
Julio Fabian <Fabian87_delarosa@outlook.com>
2023-05-01 20:11:36 -0600

    implementacion de "mantener sesion"

--- app/src/main/java/com/example/kilokapp/FragmentIniciarSesion.kt
+++ app/src/main/java/com/example/kilokapp/FragmentIniciarSesion.kt
@@ -1,6 +1,7 @@
 package com.example.kilokapp
 
-import android.content.Intent
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,8 +10,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.example.kilokapp.databinding.ActivityMainBinding
 import com.example.kilokapp.databinding.FragmentIniciarSesionBinding
+import com.example.sqlserverprototipe02.sql.SQLAcctions
+import java.sql.ResultSet
 
 
 class FragmentIniciarSesion : Fragment() {
@@ -18,6 +20,9 @@ class FragmentIniciarSesion : Fragment() {
     private lateinit var navController: NavController
     private var _binding: FragmentIniciarSesionBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sesion : SQLAcctions
+    private lateinit var preferences : SharedPreferences // mantiene la sesion iniciada.
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,13 +40,50 @@ class FragmentIniciarSesion : Fragment() {
 
         // aqui se llama al codigo de la app.
         navController = Navigation.findNavController(view) // metodo mas recomndable.
+
+        Init() // inicializamos variables globales
+
         IniciarUI()
     }
+
+    fun Init() {
+        sesion = SQLAcctions()
+        preferences = this.requireActivity().getSharedPreferences("Preferencias", MODE_PRIVATE) // creamos shared preferences
+    }
+
+    fun verificarSesionIniciada() { // si existe una sesion activa, omitimos el login.
+        // code here ...
+        val user : String? = preferences.getString("USER", null)
+        val id : Int = preferences.getInt("USER_ID", 0)
+
+        if (user != null && id > 0) {
+            navController.navigate(R.id.action_go_home)
+        }
+    }
+
 
     private fun IniciarUI() {
 
         binding.botonIniciarSesion.setOnClickListener {
-            navController.navigate(R.id.action_go_home)
+
+            var user : String = binding.editTextUsuario.text.toString()
+            var pass : String = binding.editTextPassword.text.toString()
+
+            var resultado : ResultSet? = sesion.buscarUsuarioIniciarSesion(user, pass)
+
+            if (resultado != null) {
+                val editor = preferences.edit()
+                editor.putInt("USER_ID", resultado.getString(1).toInt())
+                editor.putString("USER", resultado.getString(2).toString())
+                editor.apply()
+
+
+                Toast.makeText(context, "!Bienvenido ${user}¡ Iniciando Sesion...", Toast.LENGTH_SHORT).show()
+                navController.navigate(R.id.action_go_home) // TODO reemplazar por activity (asi funciona mejor el cerrar sesion)
+            }
+            else {
+                Toast.makeText(context, "La cuenta ingresada no existe", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.textViewRecuperarPassword.setOnClickListener {
--- app/src/main/java/com/example/kilokapp/home/FragmentExcercices.kt
+++ app/src/main/java/com/example/kilokapp/home/FragmentExcercices.kt
@@ -1,14 +1,23 @@
 package com.example.kilokapp.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kilokapp.R
+import com.example.kilokapp.databinding.FragmentExcercicesBinding
+import com.example.kilokapp.databinding.FragmentIniciarSesionBinding
+import com.example.sqlserverprototipe02.sql.SQLAcctions
 
 class FragmentExcercices : Fragment() {
 
+    private lateinit var preferences : SharedPreferences // mantiene la sesion iniciada.
+    private lateinit var sesion : SQLAcctions
+    private var _binding: FragmentExcercicesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +29,33 @@ class FragmentExcercices : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_excercices, container, false)
+        _binding = FragmentExcercicesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // code here...
+        Init()
+
+        val user : String? = preferences.getString("USER", null)
+        val id : Int = preferences.getInt("USER_ID", 0)
+
+        if (user != null && id > 0) {
+            val msg = "¡Hola ${user}! ¡bienvenido!"
+            binding.textViewEjericios.text =  msg
+        }
+
+
+    }
+
+    fun Init() {
+        sesion = SQLAcctions()
+        preferences = this.requireActivity().getSharedPreferences("Preferencias",
+            Context.MODE_PRIVATE
+        ) // creamos shared preferences
     }
 
 }
